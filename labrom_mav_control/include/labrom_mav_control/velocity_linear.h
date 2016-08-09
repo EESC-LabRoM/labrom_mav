4888544567c786f4e006d51f68f40925cadc9a52
Rômulo T Rodrigues <romulortr@gmail.com>
2016-08-09 18:04:25 -0300

    Pushing mav_control

---
+++ labrom_mav_control/include/labrom_mav_control/velocity_linear.h
@@ -0,0 +1,80 @@
+/*************************************************************************
+*   Linear plant associate with a PID control law for hovering (Header file)
+*   This file is part of labrom_mav_control
+*
+*   labrom_mav_control is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   labrom_mav_control is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with labrom_mav_control.  If not, see <http://www.gnu.org/licenses/>.
+***************************************************************************/
+
+#ifndef VELOCITY_LINEAR_H_
+#define VELOCITY_LINEAR_H_
+
+//labrom_control libraries
+#include<labrom_control/pid_simple.h>
+
+// ROS libraries
+#include "ros/ros.h"
+#include "tf/tf.h"
+// ROS message libraries
+#include "nav_msgs/Odometry.h"
+#include "trajectory_msgs/JointTrajectoryPoint.h"
+#include "std_msgs/Int32.h"
+#include "geometry_msgs/Vector3.h"
+
+#include <vector>
+
+//! top level namespace
+namespace mav_control{
+//! velocity controllers
+namespace velocity{
+//! Linearized plants
+namespace linear{
+//! Linearized quadrotor controllers for velocity commands.
+class Controller{
+  public:
+    //! Empty constructor
+    Controller(void);
+    //! Empty destructor
+    ~Controller(void);
+    //! Trajectory message callback
+    void TrajCallback(const trajectory_msgs::JointTrajectoryPoint::ConstPtr &msg);
+    //! Odometry message callback
+    void OdometryCallback(const nav_msgs::Odometry::ConstPtr &msg);
+    //! Loop
+    void Loop(void);
+
+  private:     
+    ros::NodeHandle nh_;                                    //!< ROS NodeHandle
+    ros::Subscriber traj_sub_;                              //!< trajectory subscriber
+    ros::Subscriber odom_sub_;                              //!< odometry subscriber
+    ros::Publisher thrust_pub_;                             //!< Thrust publisher
+    ros::Publisher attitude_pub_;                           //!< Attitude publisher
+
+    trajectory_msgs::JointTrajectoryPoint traj_des_;        //!< Desired trajectory message
+    std_msgs::Int32 thrust_;                                //!< thrust Newtons message
+    geometry_msgs::Vector3 attitude_;                      //!< Attitude (roll, pitch, yaw) message
+
+    controllers::pid::Simple pid_ddx_;
+    controllers::pid::Simple pid_ddy_;
+    controllers::pid::Simple pid_ddz_;
+
+    struct{
+      double mass;
+      double gravity;
+    } params_;
+
+};
+} // pid namespace
+} // linear namespace
+} // mav_control namespace
+#endif
---
+++ labrom_mav_control/nodes/velocity_linear.cpp
@@ -0,0 +1,113 @@
+/*************************************************************************
+*   Linear plant associate with a PID control law for hovering (Implemenation)
+*   This file is part of labrom_mav_control
+*
+*   labrom_mav_control is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   labrom_mav_control is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with labrom_mav_control.  If not, see <http://www.gnu.org/licenses/>.
+***************************************************************************/
+
+// labrom_mav_control libraries
+#include <labrom_mav_control/velocity_linear.h>
+
+namespace mav_control{
+namespace velocity{
+namespace linear{
+/**
+* Empty constructor
+*/
+Controller::Controller(void): nh_("~"){
+  // Publishers and subscribers
+  traj_sub_     = nh_.subscribe("/trajectory",1, & Controller::TrajCallback,this);
+  odom_sub_     = nh_.subscribe("/odometry",1, &Controller::OdometryCallback,this);
+  thrust_pub_   = nh_.advertise<std_msgs::Int32>("/cmd_thrust",1);
+  attitude_pub_ = nh_.advertise<geometry_msgs::Vector3>("/cmd_attitude",1);
+
+  // Controllers
+  std::vector<double> kp, ki, kd, anti_windup;
+  for(int i=0;i<3;++i){
+    kp.push_back(0);
+    ki.push_back(0);
+    kd.push_back(0);
+    anti_windup.push_back(0);
+  }
+
+  nh_.getParam("Kp_xyz", kp);
+  nh_.getParam("Ki_xyz", ki);
+  nh_.getParam("Kd_xyz", kd);
+  nh_.getParam("anti_windup_xyz", kd);
+
+  pid_ddx_ = controllers::pid::Simple("PID_ddx", kp[0], ki[0],  kd[0],  anti_windup[0]);
+  pid_ddy_ = controllers::pid::Simple("PID_ddy", kp[1], ki[1],  kd[1],  anti_windup[1]);
+  pid_ddz_ = controllers::pid::Simple("PID_ddz", kp[2], ki[2],  kd[2],  anti_windup[2]);
+
+  // parameters
+  nh_.param("mass", params_.mass, 1.0);
+  nh_.param("gravity", params_.gravity, 9.81);
+};
+
+/**
+* Empty destructor
+*/
+Controller::~Controller(void){};
+
+/**
+*  Trajectory message callback
+* @param msg last trajectory message received
+*/
+void Controller::TrajCallback(const trajectory_msgs::JointTrajectoryPoint::ConstPtr &msg){
+  // Save trajectory
+  traj_des_ = *msg;
+}
+
+/**
+* Odometry message callback
+*/
+void Controller::OdometryCallback(const nav_msgs::Odometry::ConstPtr &msg){
+  double now = ros::Time::now().toSec(); 
+  static double prev_time = now;
+
+  // Roll, pitch and yaw angles
+  double roll, pitch, yaw;
+  tf::Quaternion qt(msg->pose.pose.orientation.x, 
+                    msg->pose.pose.orientation.y, 
+                    msg->pose.pose.orientation.z, 
+                    msg->pose.pose.orientation.w);
+  tf::Matrix3x3 R(qt);
+  R.getRPY(roll, pitch, yaw);
+
+  // Transform velocities from body-fixed frame to speed frame
+  double vx = cos(pitch)* msg->twist.twist.linear.x + sin(roll)*sin(pitch)* msg->twist.twist.linear.y + cos(roll)*sin(pitch)* msg->twist.twist.linear.z;
+  double vy = cos(roll) * msg->twist.twist.linear.y - sin(roll)* msg->twist.twist.linear.z;
+  double vz = -sin(pitch) * msg->twist.twist.linear.x + cos(pitch)*sin(roll)*msg->twist.twist.linear.y + cos(roll)*cos(pitch)*msg->twist.twist.linear.z;
+  
+  // Command accelerations
+  double dt = now - prev_time;
+  double ddx_c = pid_ddx_.LoopOnce(traj_des_.velocities[0], vx, dt);
+  double ddy_c = pid_ddy_.LoopOnce(traj_des_.velocities[1], vy, dt);
+  double ddz_c = pid_ddz_.LoopOnce(traj_des_.velocities[2], vz, dt);
+
+  // Quadrotor input commands
+  double T_d     = (params_.gravity - ddz_c)/params_.mass;
+  double roll_d  = (params_.mass)/T_d * ddy_c;
+  double pitch_d = (params_.mass)/T_d * ddx_c;
+ 
+  //! @todo FINISH controller
+  prev_time = now;
+}
+
+//! Loop
+void Controller::Loop(void){};
+
+} // pid namespace
+} // linear namespace
+} // mav_control namespace
