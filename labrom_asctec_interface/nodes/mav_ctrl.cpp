22bad511f4ae36726bc4e67f9f86114f2d2bf677
romulortr <romulortr@gmail.com>
2016-08-27 18:29:28 -0300

    Create mav_ctrl.cpp

---
+++ labrom_asctec_interface/nodes/mav_ctrl.cpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+*   Transform commands from labrom to asctec_hl_interface format (node implementation)
+*   This file is part of labrom_asctec_interface
+*
+*   labrom_asctec_interface is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   labrom_asctec_interface is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with labrom_asctec_interface.  If not, see <http://www.gnu.org/licenses/>.
+***************************************************************************/
+
+#include "labrom_asctec_interface.h/mav_ctrl.h"
+
+namespace labrom_asctec_interface{
+
+/**
+* Empty constructor.
+*/
+CtrlNode::CtrlNode(void): nh_("~"){};
+
+/**
+* Empty destructor.
+*/
+CtrlNode::CtrlNode(void){};
+
+} // labrom_asctec_interface namespace
