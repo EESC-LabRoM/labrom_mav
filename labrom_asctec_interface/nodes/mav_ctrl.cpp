e8cf917725bfcb33b076a1ba3cda195a224e3f94
romulortr <romulortr@gmail.com>
2016-08-28 10:01:32 -0300

    Update mav_ctrl.cpp

--- labrom_asctec_interface/nodes/mav_ctrl.cpp
+++ labrom_asctec_interface/nodes/mav_ctrl.cpp
@@ -23,7 +23,18 @@ namespace labrom_asctec_interface{
 /**
  * Empty constructor.
  */
-CtrlNode::CtrlNode(void): nh_("~"){};
+CtrlNode::CtrlNode(void): nh_("~"){
+  // Paramas
+  nh_.param("max_thrust", _max_thrust, 17.1675);
+
+  // Start subscribers
+  sub_thrust_   = node.subscribe("/cmd_thrust"  , 1, thrustCallback);
+  sub_attitude_ = node.subscribe("/cmd_attitude", 1, attitudeCallback);
+  
+  // Start publishers
+  pub_mav_ctrl_ = node.advertise<asctec_hl_comm::mav_ctrl>("control", 1);
+
+};
 
 /**
  * Empty destructor. 
@@ -43,5 +54,35 @@ void CtrlNode::ThrustCallback(const std_msgs::Int32::ConstPtr &msg){
 void CtrlNode::AttitudeCallback(const geometry_msgs::Vector3::ConstPtr &msg){
   attitude_ = *msg;
 }
+
+/**
+ * Publish control message in asctec_hl_interface format
+ */
+void CtrlNode::PublishMavCtrl(void){
+    // Control method
+    switch (msg_mav_ctrl.type){
+      case 1: // x~pitch, y~roll, z~thrust, units in rad and rad/s for yaw
+              msg_mav_ctrl.x = attitude.y;                  // pitch
+              msg_mav_ctrl.y = attitude.x;                  // roll
+              msg_mav_ctrl.z = thrust.data / _max_thrust;   // yaw
+              msg_mav_ctrl.yaw = attitude.z;    
+              break; 
+      case 2: //  x, y, z, yaw correspond to x_dot, y_dot, ...
+              msg_mav_ctrl.x = attitude.y;                  // pitch
+              msg_mav_ctrl.y = attitude.x;                  // roll
+              msg_mav_ctrl.z = thrust.data / _max_thrust;   // yaw
+              msg_mav_ctrl.yaw = attitude.z;    
+              break;      
+      default:msg_mav_ctrl.x = 0;   
+              msg_mav_ctrl.y = 0;   
+              msg_mav_ctrl.z = 0;   
+              msg_mav_ctrl.yaw = 0;
+              break;
+    }
+    // Publish message
+    pub_mav_ctrl.publish(msg_mav_ctrl);
+}
+
+
 
 } // labrom_asctec_interface namespace
