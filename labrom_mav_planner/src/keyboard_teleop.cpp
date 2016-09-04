d81e7e829ea37791478d56c55aa2d8dcde52e0e3
romulortr <romulortr@gmail.com>
2016-09-04 18:33:43 -0300

    Create keyboard_teleop.cpp

---
+++ labrom_mav_planner/src/keyboard_teleop.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+*   Keyboard tele operation implementation
+*   This file is part of labrom_mav_planner
+*
+*   labrom_mav_planner is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   labrom_mav_planner is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with labrom_mav_planner.  If not, see <http://www.gnu.org/licenses/>.
+***************************************************************************/
