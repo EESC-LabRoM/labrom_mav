be3697ba779dc474edc8ce4111212437a902f4a1
romulortr <romulortr@gmail.com>
2016-09-04 18:36:54 -0300

    Create keyboard_teleop.h

---
+++ labrom_mav_planner/include/labrom_mav_planner/keyboard_teleop.h
@@ -0,0 +1,29 @@
+/*************************************************************************
+*   Keyboard tele operation header files
+*   This file is part of labrom_mav_planner
+*
+*   labrom_mav_planner is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   labrom_mav_planner is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with labrom_mav_planner.  If not, see <http://www.gnu.org/licenses/>.
+***************************************************************************/
+#ifndef KEYBOARD_TELEOP_H_
+#define KEYBOARD_TELEOP_H_
+
+namespace mav_planner{
+namespace teleop{
+class Keyboard{
+
+};
+} // teleop namespace
+} // mav_planner namespace
+
+#end
